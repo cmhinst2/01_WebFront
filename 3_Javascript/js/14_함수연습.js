20d142d759dba2721235eba60c89ced6d9dfa5bd
cmhinst2 <cmhinst2@gmail.com>
2024-08-26 16:48:44 +0900

    학원에서 함수파트 끝

--- 3_Javascript/js/14_함수연습.js
+++ 3_Javascript/js/14_함수연습.js
@@ -158,3 +158,6 @@ function sayGoodbye(name) {
 // callback 함수 sayHello와 sayGoodbye
 greet('길동', sayHello);  // 안녕하시오, 길동!
 greet('미현', sayGoodbye);  // 잘가, 미현!
+
+
+console.log("git Test 중~ 학원에서 작성");
